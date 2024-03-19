6e99fd4428f78e6db95d2e37005c951d2b07aacb
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2024-03-19 09:51:29 +0100

    :tada: Initial commit

---
+++ jsdoc-demo/src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./style.css");
+/**
+ * A function to present a greeting to the user
+ * @param {string} message - The message to show
+ */
+var greeting = function (message) {
+    alert(message);
+};
+greeting("Hello world");
---
+++ jsdoc-demo/src/main.ts
@@ -0,0 +1,11 @@
+import "./style.css";
+
+/**
+ * A function to present a greeting to the user
+ * @param {string} message - The message to show
+ */
+const greeting = (message: string) => {
+  alert(message);
+};
+
+greeting("Hello world");
